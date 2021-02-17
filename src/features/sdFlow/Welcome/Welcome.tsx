5285841a6624becec88e166d0a8ccc7ba13d630b
Casper Engelmann <me@casperengelmann.com>
2021-02-17 12:56:24 +0100

    I know what I am doing. Trust me.

--- src/features/sdFlow/Welcome/Welcome.tsx
+++ src/features/sdFlow/Welcome/Welcome.tsx
@@ -10,59 +10,80 @@ interface Props {
 }
 
 const Welcome: FunctionComponent<Props> = ({ className }): JSX.Element => {
-  const rootClassName = clsx(s.root, {}, 'mx-auto max-w-3xl', className)
+  const rootClassName = clsx(s.root, {}, className)
 
   return (
     <motion.div exit={{ opacity: 0 }} className={rootClassName}>
-      <div className="space-y-6">
-        <h1 className="text-4xl font-bold leading-tight">Bliv medlem i dag</h1>
+      <div className="mx-auto max-w-3xl space-y-16">
+        <div className="space-y-6">
+          <h1 className="text-4xl font-bold leading-tight">
+            Bliv medlem i dag
+          </h1>
 
-        <p className="text-base leading-tight">
-          Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum
-          nibh, ut fermentum massa justo sit amet risus. Donec ullamcorper nulla
-          non metus auctor fringilla. Vivamus sagittis lacus vel augue laoreet
-          rutrum faucibus dolor auctor.
-        </p>
-      </div>
+          <p className="text-base leading-tight">
+            Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum
+            nibh, ut fermentum massa justo sit amet risus. Donec ullamcorper
+            nulla non metus auctor fringilla. Vivamus sagittis lacus vel augue
+            laoreet rutrum faucibus dolor auctor.
+          </p>
+        </div>
 
-      <div className="space-y-6">
-        <h2 className="text-sm leading-tight">I flowet skal du følgende</h2>
+        <div className="space-y-6">
+          <h2 className="text-2xl font-bold leading-tight">
+            I flowet skal du følgende
+          </h2>
 
-        <div className="relative">
-          <div className="absolute inset-0">
-            <Line className="w-full"></Line>
-          </div>
+          <div className="relative">
+            <div className="absolute inset-0 px-28 transform -translate-y-4">
+              <Line className="w-full"></Line>
+            </div>
 
-          <div className="relative grid grid-cols-3">
-            <figure className="flex flex-col items-center">
-              <img src="/img/umbrella.png" alt="umbrella" />
-              <figcaption className="text-center text-sm">
-                Vælge hvordan du og dine børn ønsker at blive dækket
-              </figcaption>
-            </figure>
+            <div className="relative grid grid-cols-3">
+              <figure className="flex flex-col items-center">
+                <div className="h-32">
+                  <img
+                    src="/img/umbrella.png"
+                    alt="umbrella"
+                    className="w-full"
+                  />
+                </div>
+                <figcaption className="text-center text-sm">
+                  Vælge hvordan du og dine børn ønsker at blive dækket
+                </figcaption>
+              </figure>
 
-            <figure className="flex flex-col items-center">
-              <img src="/img/phone.png" alt="phone" />
-              <figcaption className="text-center text-sm">
-                Personlige oplysninger
-              </figcaption>
-            </figure>
+              <figure className="flex flex-col items-center">
+                <div className="h-32">
+                  <img src="/img/phone.png" alt="phone" className="w-full" />
+                </div>
+                <figcaption className="text-center text-sm">
+                  Personlige oplysninger
+                </figcaption>
+              </figure>
 
-            <figure className="flex flex-col items-center">
-              <img src="/img/medical.png" alt="medical" />
-              <figcaption className="text-center text-sm">
-                Helbredsoplysninger
-              </figcaption>
-            </figure>
+              <figure className="flex flex-col items-center">
+                <div className="h-32">
+                  <img
+                    src="/img/medical.png"
+                    alt="medical"
+                    className="w-full"
+                  />
+                </div>
+                <figcaption className="text-center text-sm">
+                  Helbredsoplysninger
+                </figcaption>
+              </figure>
+            </div>
           </div>
         </div>
-      </div>
 
-      <div className="flex flex-col items-start">
-        <button className="inline-flex px-6 py-3 bg-sd-primary hover:bg-sd-primary-hover rounded-full space-x-2">
-          <span>Gå i gang</span>
-          <ArrowRight></ArrowRight>
-        </button>
+        <div className="flex flex-col items-center space-y-6">
+          <button className="inline-flex px-6 py-3 bg-sd-primary hover:bg-sd-primary-hover rounded-full shadow-lg hover:shadow-xl space-x-2 transition duration-100 ease-in-out">
+            <span>Gå i gang</span>
+            <ArrowRight></ArrowRight>
+          </button>
+          <button className="underline">Gå til kladde</button>
+        </div>
       </div>
     </motion.div>
   )
