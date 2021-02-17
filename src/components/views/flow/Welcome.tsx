f326e31cf922f9ee81399c37973f318c0a5b0373
Casper Engelmann <me@casperengelmann.com>
2021-02-17 10:47:19 +0100

    I dont know what I am doing

---
+++ src/components/views/flow/Welcome.tsx
@@ -0,0 +1,13 @@
+export const Welcome = (): JSX.Element => {
+  return (
+    <div>
+      <h1 className="text-4xl">Bliv medlem i dag?</h1>
+      <p>
+        Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh,
+        ut fermentum massa justo sit amet risus. Donec ullamcorper nulla non
+        metus auctor fringilla. Vivamus sagittis lacus vel augue laoreet rutrum
+        faucibus dolor auctor.
+      </p>
+    </div>
+  )
+}
